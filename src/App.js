f204f442beaca24bf752afb5a74ede1e02d7dfa8
pranavv481 <pranavv481@gmail.com>
2020-05-28 08:26:50 +0530

    useCustomHook

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import DocTitleOne from './components/DocTitleOne';
+import DocTitleTwo from './components/DocTitleTwo';
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <DocTitleOne />
+      <DocTitleTwo />
+    </div >
+
+  );
+}
+
+export default App;
